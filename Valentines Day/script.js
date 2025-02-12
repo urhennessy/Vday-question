8e9d7005896155391afbe9543061689a4b82e707
urhennessy <ryspayev.a500@gmail.com>
2025-02-13 03:31:42 +0500

    Add files via upload

---
+++ Valentines Day/script.js
@@ -0,0 +1,51 @@
+const container = document.getElementById("container");
+const imageOne = document.querySelector(".image-1");
+const imageTwo = document.querySelector(".image-2");
+const imageThree = document.querySelector(".image-3");
+const imageFour = document.querySelector(".image-4");
+const imageFive = document.querySelector(".image-5");
+const imageSix = document.querySelector(".image-6");
+const imageSeven = document.querySelector(".image-7");
+const btnYes = document.querySelector(".btn-yes");
+const btnNo = document.querySelector(".btn-no");
+
+function getRandomNumber(min, max) {
+  // Calculate the random number between min and max (inclusive)
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return randomNumber;
+}
+
+btnNo.addEventListener("click", (e) => {
+  const containerHeight = container.getBoundingClientRect().height;
+  const containerWidth = container.getBoundingClientRect().width;
+  const btnHeight = btnNo.getBoundingClientRect().height;
+  const btnWidth = btnNo.getBoundingClientRect().width;
+  const btnTop = btnNo.getBoundingClientRect().top;
+  const btnLeft = btnNo.getBoundingClientRect().left;
+
+  let newTop = btnTop;
+  let newLeft = btnLeft;
+  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
+    newTop = getRandomNumber(0, containerHeight - btnHeight);
+  }
+
+  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
+    newLeft = getRandomNumber(0, containerWidth - btnWidth);
+  }
+
+  btnNo.style.top = Math.floor(newTop) + "px";
+  btnNo.style.left = Math.floor(newLeft) + "px";
+});
+
+btnYes.addEventListener("click", (e) => {
+  imageFour.classList.remove("hide");
+  imageFive.classList.remove("hide");
+  imageSix.classList.remove("hide");
+  imageSeven.classList.remove("hide");
+  btnNo.classList.add("hide");
+  btnYes.classList.add("hide");
+  imageOne.classList.add("hide");
+  imageTwo.classList.remove("hide");
+  imageThree.classList.remove("hide");
+});
